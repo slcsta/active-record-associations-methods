b28485c4eb1fb498525128a796f04c9822623dfc
Sarah <sarahlchavez@gmail.com>
2021-05-05 10:29:01 -0700

    commit tree f1282a4118e7f47ccecc3209094cb85810ecf144

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,18 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    #Artist (get first song - from db so songs) .songs.first
+    #get instance's genre so .genre
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count 
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.count
   end
 end
